582af7a507d9c8d6fc87725b9a14b65db59a269d
Szymon1620 <166690616+Szymon1620@users.noreply.github.com>
2024-04-18 10:50:26 +0200

    wip:refactoring folders

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from utils.crud import show_users
 
 
 
-
-
 if __name__ == "__main__":
     print("Witaj użytkowniku")
     while True:
-
         print("Menu:")
+        print("0. zakończ program")
         print("1. Wyświetl co u znajomych")
+        print("2. dodaj użytkownika")
         menu_option: str = input("dokonaj wyboru:")
         if menu_option == "0":
             print("program kończy pracę")
             break
         if menu_option == "1":
             show_users(users)
+        if menu_option == "2":
+            add_new_user(users)
